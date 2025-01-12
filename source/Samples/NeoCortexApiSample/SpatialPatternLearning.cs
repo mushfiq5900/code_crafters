475d834591c81cbdebbe08b5d366bdfc87ce274e
Rezoyana Islam Bonna <ribonna6830@gmail.com>
2025-01-12 23:16:00 +0100

    Again tried 1000 learning cycles

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -130,7 +130,7 @@ namespace NeoCortexApiSample
 
             // It creates the instance of Spatial Pooler Multithreaded version.
             SpatialPooler sp = new SpatialPooler(hpa);
-            sp = new SpatialPoolerMT(hpa);
+            //sp = new SpatialPoolerMT(hpa);
 
             // Initializes the 
             sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) });
@@ -167,7 +167,7 @@ namespace NeoCortexApiSample
             }
 
             // Learning process will take 1000 iterations (cycles)
-            int maxSPLearningCycles = 500;
+            int maxSPLearningCycles = 1000;
 
             int numStableCycles = 0;
 
