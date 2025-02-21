347fb45d36d5583e2e2e4028fa7c301df2f4a582
Proloy11 <anonto321@gmail.com>
2025-02-21 23:48:31 +0100

    Generating Heatmap with diff matrix value

--- source/NeoCortexUtils/NeoCortexUtils.cs
+++ source/NeoCortexUtils/NeoCortexUtils.cs
@@ -413,7 +413,7 @@ namespace NeoCortex
         }
 
         //Saving Heatmap Values as Image
-        public static void SaveHeatmapValuesAsImage(List<double> data, string filePath, int rows = 8, int cols = 25, int scaleFactor = 50)
+        public static void SaveHeatmapValuesAsImage(List<double> data, string filePath, int rows = 4, int cols = 50, int scaleFactor = 50)
         {
             if (data == null || data.Count == 0)
                 throw new ArgumentException("Heatmap data is empty.");
@@ -422,7 +422,7 @@ namespace NeoCortex
                 throw new ArgumentException($"Data length does not match the expected {rows}x{cols} matrix size.");
             double minVal = data.Min();
             double maxVal = data.Max();
-            //int imgWidth = cols * scaleFactor;
+            int imgWidth = cols * scaleFactor;
             int imgHeight = rows * scaleFactor;
             using (Bitmap bitmap = new Bitmap(imgWidth, imgHeight))
             using (Graphics graphics = Graphics.FromImage(bitmap))
