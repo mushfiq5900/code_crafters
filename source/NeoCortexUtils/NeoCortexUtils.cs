15163085f664224ce012630aca7f3cee85c8906c
Mushfiq <mushfiquli@gmail.com>
2025-03-12 17:38:32 +0100

    Comparing changes with previous values

--- source/NeoCortexUtils/NeoCortexUtils.cs
+++ source/NeoCortexUtils/NeoCortexUtils.cs
@@ -438,8 +438,8 @@ namespace NeoCortex
                         int index = i * cols + j;
                         double normalized = (data[index] - minVal) / (maxVal - minVal);
                         string text = normalized.ToString("F1");
-                        int x = j * scaleFactor + 2;
-                        int y = i * scaleFactor + 2;
+                        int x = j * scaleFactor + 5;
+                        int y = i * scaleFactor + 5;
                         graphics.DrawString(text, font, textBrush, new PointF(x, y));
                     }
                 }
